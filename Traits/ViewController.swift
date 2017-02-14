72cc7d452b58b83884e0f979f586a2aa95910145
Imanou Petit <imanoupetit@yahoo.fr>
2017-02-14 10:10:11 +0100

    Implement `traitCollectionDidChange(_:)` instead of `viewWillLayoutSubviews()`

--- Traits/ViewController.swift
+++ Traits/ViewController.swift
@@ -19,15 +19,41 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
     }
     
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        if previousTraitCollection?.verticalSizeClass != traitCollection.verticalSizeClass {
+            switch traitCollection.verticalSizeClass {
+            case .compact:
+                blueView.constraints.forEach({ $0.isActive = false })
+                blueView.translatesAutoresizingMaskIntoConstraints = false
+                self.tableView.tableHeaderView = nil
+            case .unspecified:
+                fallthrough
+            case .regular:
+                blueView.constraints.forEach({ $0.isActive = false })
+                blueView.translatesAutoresizingMaskIntoConstraints = true
+                blueView.bounds = CGRect(x: tableView.frame.origin.x, y: tableView.frame.origin.y, width: tableView.bounds.width, height: 50)
+                tableView.tableHeaderView = blueView
+            }
+        }
+    }
+
+    /*
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
         if self.traitCollection.verticalSizeClass == .regular {
+            blueView.constraints.forEach({ $0.isActive = false })
+            blueView.translatesAutoresizingMaskIntoConstraints = true
             blueView.bounds = CGRect(x: tableView.frame.origin.x, y: tableView.frame.origin.y, width: tableView.bounds.width, height: 50)
             tableView.tableHeaderView = blueView
         } else {
+            blueView.constraints.forEach({ $0.isActive = false })
+            blueView.translatesAutoresizingMaskIntoConstraints = false
             self.tableView.tableHeaderView = nil
         }
     }
+    */
     
 }
